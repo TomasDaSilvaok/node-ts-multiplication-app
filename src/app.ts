8ba93836248f8663ed73cc3c6fe98cbbcb1c0a94
tomadsilva <tomasdaniel.dasilva@gmail.com>
2025-11-10 15:03:50 -0300

    updated app.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import { ServerApp } from "./presentation/server-app";
 import { yarg } from "./config/plugins/args.plugin";
 
 (async () => {
+  await main();
+})();
+
+function main() {
   const { b: base, l: limit, s: showTable, d: destination, n: name } = yarg;
 
   ServerApp.run({ base, limit, showTable, destination, name });
-})();
+}
